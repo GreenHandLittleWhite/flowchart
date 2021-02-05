2b227eb556bb7807b8458efaafae120506a26cb3
GreenHandLittleWhite <a3737337@gmail.com>
2021-02-05 09:58:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app"></div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
+
+<style lang="less">
+body,
+div {
+    margin: 0;
+    padding: 0;
+}
+html {
+    height: 100%;
+}
+body {
+    height: 100%;
+    font-family: sans-serif;
+    color: #666;
+    line-height: 1.5;
+}
+</style>
